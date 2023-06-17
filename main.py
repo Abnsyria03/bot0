9fcb77fc560da400dcb9baeb44b40dea48a7c400
Abnsyria03 <134682323+Abnsyria03@users.noreply.github.com>
2023-06-17 20:55:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyrogram , pyromod
+
+from pyromod import listen
+from keep import alive
+from pyrogram import Client, filters, enums
+p = dict(root='plugins')
+from kvsqlite.sync import Client
+
+db = Client("data.sqlite", 'fuck')
+if not db.exists("admin_list"):
+    db.set('admin_list', [5108562302,5108562302])
+if not db.exists("sessions"):
+    db.set('sessions', [])
+if not db.exists("ban_list"):
+    db.set("ban_list", [])
+x = Client(name='lossclhos', api_id=19312827, api_hash='84da7f08e87849853b2fa6728e4192a2', bot_token='5496059752:AAG1MGGY-lSDEd9ADAdhAr9xVDDuPlwuTWE', workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
+alive()
+x.run()
