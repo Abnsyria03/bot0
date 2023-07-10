3496cac924073a4f27b63b89707c4abaa3682bf6
Abnsyria03 <134682323+Abnsyria03@users.noreply.github.com>
2023-07-10 09:37:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ if not db.exists("sessions"):
     db.set('sessions', [])
 if not db.exists("ban_list"):
     db.set("ban_list", [])
-x = Client(name='lossclhos', api_id=19312827, api_hash='84da7f08e87849853b2fa6728e4192a2', bot_token='5455927541:AAEh9x4Zfk3gq2gQB1UpT39FTi45dY8bCa0', workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
+x = Client(name='lossclhos', api_id=19312827, api_hash='84da7f08e87849853b2fa6728e4192a2', bot_token='5455927541:AAGg2yoyx-1eaOkFxgYCsqybiFg0LYeP6mU', workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
 alive()
 x.run()
